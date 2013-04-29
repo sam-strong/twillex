566a3a40b7ec0b84774c5fcfbabeedffd9066838
samuel strong <s.j.p.strong@gmail.com>
2013-04-29 16:18:06 +0100

    set up rails repo to work with postgres and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twillex::Application.config.secret_token = '94e49db7dfc1a4435ce371ee4b898e12f2a91f0f01e260793e8b2658d235f6c58a98cba01cc0065b92aa56778450fd4e8b27e6ab26ecb44e5a4665bc0328f9a2'
