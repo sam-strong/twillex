106b8412fefa28a1a0acd388ff89285fa7c4b0d0
newton101 <miguil@hotmail.co.uk>
2013-04-30 16:49:21 +0100

    finish api testing

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ end
 gem 'jquery-rails'
 
 gem 'open_exchange_rates'
+gem 'curb'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- spec/exchange/providers/open_exchange_rates_spec.rb
+++ spec/exchange/providers/open_exchange_rates_spec.rb
@@ -1,29 +1,41 @@
 require 'spec_helper'
 
 describe 'OpenExchangeRates' do
-  let(:rates) { OpenExchangeRates::Rates.new }
+  before(:all) do
+    @rates = OpenExchangeRates::Rates.new
+  end
 
   it 'converts from USD to GBP' do
-    result = rates.convert 123.45, from: 'USD', to: 'GBP'
+    result = @rates.convert 123.45, from: 'USD', to: 'GBP'
     puts result
     result.should be_a_kind_of Numeric
   end
 
   it 'converts from GBP to EUR' do
-    result = rates.convert 123.45, from: 'GBP', to: 'EUR'
+    result = @rates.convert 123.45, from: 'GBP', to: 'EUR'
     puts result
     result.should be_a_kind_of Numeric
   end
 
   it 'converts from EUR to AED' do
-    result = rates.convert 123.45, from: 'EUR', to: 'AED'
+    result = @rates.convert 123.45, from: 'EUR', to: 'AED'
     puts result
     result.should be_a_kind_of Numeric
   end
 end
 
 describe 'openexchangerates api' do
-  it 'gets a list of currencies' do
+  before(:all) do
+    @response = Curl::Easy.perform("http://openexchangerates.org/currencies.json?api_id=#{OpenExchangeRates.configuration.app_id}")
+  end
 
+  it 'gets a list of currencies' do
+    @response.status.should eq "200 OK"
+  end
+
+  it 'contains GBP' do
+    currencies = JSON.parse @response.body_str
+    puts currencies.class
+    currencies.has_key?("GBP").should be_true
   end
 end
