af97ffb6acd9425a56430091f797b1df49c82cbd
Zahid <kronosmes@gmail.com>
2013-04-30 15:43:52 +0100

    controller generated

---
+++ app/controllers/exchanges_controller.rb
@@ -0,0 +1,3 @@
+class ExchangeController < ApplicationController
+
+end
---
+++ spec/controllers/exchanges_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ExchangesController do
+  describe 'POST exchanges#create' do
+    it 'converts a currency value' do
+
+
+    end
+  end
+
+
+end
+
