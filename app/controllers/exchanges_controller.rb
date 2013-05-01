5302d7309d80a5b384c39b94fe7e7c0988554de2
samuel strong <s.j.p.strong@gmail.com>
2013-05-01 17:28:54 +0100

    fix to exchange controller

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,9 +2,9 @@ class ExchangesController < ApplicationController
 
   def create
     @exchange = Exchange.from_config
-    @exchange.convert(params['amount'].to_i, params['from_currency'], params['to_currency'])
+    @amount = @exchange.convert(params['amount'].to_i, params['from_currency'], params['to_currency'])
 
-    render :json => { :amount => @exchange }
+    render :json => { :amount => @amount }
   end
 end
 
