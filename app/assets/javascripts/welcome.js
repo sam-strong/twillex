6b5b3950c55015a3a84592ee89e71f601a8b20f0
Ruben Kostucki <rubenkostucki@gmail.com>
2013-05-01 17:46:04 +0100

    alert when amount is not a number

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,40 @@
+$( document ).ready(function(){
+
+  $("#amount").blur(function(){
+    var value = $(this).val();
+    if ($.isNumeric(value) == false) {
+    alert('please enter a valid number');
+    }
+  });
+});
+
+
+
+
+//   validate(function(e) {
+//     e.preventDefault();
+//     alert("thanks for visiting");
+//   });
+// });
+
+
+// function ValideInteger(value) {
+//   if (value.)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#amount").keyup(function(){
+//   var value =
+
+
+// })
