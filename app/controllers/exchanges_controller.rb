ee0c14421b3568eaa9fc4f80eae5ecb4452d2504
Zahid <kronosmes@gmail.com>
2013-05-02 11:53:26 +0100

    ajax fixed to output json

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,13 +3,8 @@ class ExchangesController < ApplicationController
   def create
     @exchange = Exchange.convert(params['amount'].to_i, params['from_currency'], params['to_currency'])
 
-    respond_to do |format|
-      # format.json  { render json: :amount => @exchange }
-      format.js {}
-    end
 
-
-    # render :json => { :amount => @exchange }
+    render :json => { :amount => @exchange }
 
   end
 
