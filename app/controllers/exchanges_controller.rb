72133a9de3dc5e3d14fb37e8ac0862b912112424
Zahid <kronosmes@gmail.com>
2013-05-02 10:44:36 +0100

    ajax needs fixing

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,11 +2,16 @@ class ExchangesController < ApplicationController
 
   def create
     @exchange = Exchange.convert(params['amount'].to_i, params['from_currency'], params['to_currency'])
-    # respond_to do |format|
-    #   format.json { render :json => { :amount => @exchange } }
-    # end
-    render :json => { :amount => @exchange }
+
+    respond_to do |format|
+      # format.json  { render json: :amount => @exchange }
+      format.js {}
+    end
+
+
+    # render :json => { :amount => @exchange }
 
   end
+
 end
 
