35fb4865aaac8eb2eddc965b9ff725441f93b6ce
Leo Allen <leo.f.allen@gmail.com>
2013-05-02 14:50:39 +0100

    Improved phone number validation so we can have a number with spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
 
   attr_accessible :from_currency, :name, :phone_number, :to_currency
 
-  validates :phone_number, :format => { :with => /^[-+]?[0-9]+$/,
+  validates :phone_number, :format => { :with => /^\+?[0-9\s]+$/,
     :message => "Only numbers allowed" },
     :presence => true
 
-   validates :from_currency,:to_currency,    :inclusion => { :in => Exchange::CURRENCIES,
+   validates :from_currency, :to_currency, :inclusion => { :in => Exchange::CURRENCIES,
     :message => "%{value} is not a valid currency" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  it { should validate_presence_of(:phone_number) }
-  it { should_not allow_value("foo").for(:phone_number).with_message("Only numbers allowed") }
-  it { should allow_value("324124").for(:phone_number) }
+
+  context 'given a phone number' do
+    context 'which is valid' do
+      it { should validate_presence_of(:phone_number) }
+      it { should allow_value("07234567890").for(:phone_number) }
+      it { should allow_value("+44 020 3241 4324").for(:phone_number) }
+    end
+
+    context "which is invalid" do
+      it { should_not allow_value("foo").for(:phone_number).with_message("Only numbers allowed") }
+    end
+  end
+
   it { should ensure_inclusion_of(:from_currency).in_array(Exchange::CURRENCIES) }
   it { should ensure_inclusion_of(:to_currency).in_array(Exchange::CURRENCIES) }
 
