1c9a1841a67f19fb6712b74783a34ba8a19ac5b0
samuel strong <s.j.p.strong@gmail.com>
2013-05-03 15:55:26 +0100

    rspec mocking test passes

--- spec/controllers/exchanges_controller_spec.rb
+++ spec/controllers/exchanges_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe ExchangesController do
   describe 'POST exchanges#create' do
     it 'converts a value' do
+      exchange = double(:exchange)
+      exchange.should_receive(:convert).and_return(4.5)
+
+      Exchange.should_receive(:from_config).and_return(exchange)
 
       post :create, {:amount => 3, :from_currency => "GBP", :to_currency => "USD" }
       puts response.body
