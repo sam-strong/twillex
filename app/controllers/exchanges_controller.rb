d2173d480c7832e10fcdd0c353f8f32c5cce984b
samuel strong <s.j.p.strong@gmail.com>
2013-05-03 11:21:24 +0100

    rspec tests now pass

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -2,10 +2,10 @@ class ExchangesController < ApplicationController
 
   def create
 
-    @exchange = Exchange.convert(params['amount'].to_i, params['from'], params['to'])
+    @exchange = Exchange.convert(params[:amount].to_i, params[:from_currency], params[:to_currency])
 
 
-    render :json => { amount: @exchange, to: params['to']}
+    render :json => { amount: @exchange, to_currency: params[:to_currency]}
 
 
   end
--- spec/controllers/exchanges_controller_spec.rb
+++ spec/controllers/exchanges_controller_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe ExchangesController do
   describe 'POST exchanges#create' do
     it 'converts a value' do
-      post :create, {'amount' => 3, "from_currency" => "GBP", "to_currency" => "USD" }
+      post :create, {:amount => 3, :from_currency => "GBP", :to_currency => "USD" }
       puts response.body
-      response.body.should eq '{"amount":4.5}'
+      response.body.should eq '{"amount":4.5,"to_currency":"USD"}'
 
 
     end
